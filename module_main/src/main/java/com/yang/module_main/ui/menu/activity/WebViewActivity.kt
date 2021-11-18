d19ab0385262268a6570b046ff5076de6b795722
yxy <1941008232@qq.com>
2021-11-18 17:29:49 +0800

    feat:搬砖

--- module_main/src/main/java/com/yang/module_main/ui/menu/activity/WebViewActivity.kt
+++ module_main/src/main/java/com/yang/module_main/ui/menu/activity/WebViewActivity.kt
@@ -41,16 +41,17 @@ class WebViewActivity : BaseActivity() {
     private fun initWebView() {
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(p0: WebView?, p1: String?): Boolean {
-                webView.loadUrl(p1)
-                return true
+                return super.shouldOverrideUrlLoading(p0, p1)
             }
             override fun onPageStarted(p0: WebView?, p1: String?, p2: Bitmap?) {
                 super.onPageStarted(p0, p1, p2)
+                mLoadingProgressView.loadStart = true
                 Log.i(TAG, "onPageStarted: 加载开始 $p1  $p2")
             }
 
             override fun onPageFinished(p0: WebView?, p1: String?) {
                 super.onPageFinished(p0, p1)
+                mLoadingProgressView.loadStart = false
                 Log.i(TAG, "onPageFinished: 加载完成 $p1")
             }
 
@@ -62,27 +63,28 @@ class WebViewActivity : BaseActivity() {
         webView.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(p0: WebView?, p1: Int) {
                 super.onProgressChanged(p0, p1)
+                mLoadingProgressView.mProgress = p1
                 Log.i(TAG, "onProgressChanged: $p1")
             }
         }
 
 
 
-        webView.loadUrl("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=%E8%85%BE%E8%AE%AFX5%E5%86%85%E6%A0%B8WebView&fenlei=256&oq=Android%2520%25E8%2585%25BE%25E8%25AE%25AFwebview%25E6%25A1%2586%25E6%259E%25B6&rsv_pq=da5d2c480000361b&rsv_t=adaa0CfLjh812ANiWVD6IrUmzwKPhGB7x9gg4%2BSASs2JrBN3BywArgC8C0g&rqlang=cn&rsv_enter=1&rsv_dl=tb&rsv_btype=t&inputT=258&rsv_n=2&rsv_sug3=33&rsv_sug2=0&rsv_sug4=258")
-//        webView.settings.apply {
-//            javaScriptEnabled = true
-//            useWideViewPort = true
-//            loadWithOverviewMode = true
-//            setSupportZoom(true)
-//            builtInZoomControls = true
-//            displayZoomControls = false
-//            pluginsEnabled = true
-//            cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK//关闭webview中缓存
-//            allowFileAccess = true //设置可以访问文件
-//            javaScriptCanOpenWindowsAutomatically = true //支持通过JS打开新窗口
-//            loadsImagesAutomatically = true //支持自动加载图片
-//            defaultTextEncodingName = "utf-8"//设置编码格式
-//        }
+        webView.loadUrl("https://www.baidu.com/")
+        webView.settings.apply {
+            javaScriptEnabled = true
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            setSupportZoom(true)
+            builtInZoomControls = true
+            displayZoomControls = false
+            pluginsEnabled = true
+            cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK//关闭webview中缓存
+            allowFileAccess = true //设置可以访问文件
+            javaScriptCanOpenWindowsAutomatically = true //支持通过JS打开新窗口
+            loadsImagesAutomatically = true //支持自动加载图片
+            defaultTextEncodingName = "utf-8"//设置编码格式
+        }
 
 
         Log.i(TAG, "initWebView: ${webView.settingsExtension == null}   ${webView.x5WebViewExtension == null}")
